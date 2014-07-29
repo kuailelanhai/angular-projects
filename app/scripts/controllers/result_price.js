3e584b0980ea5b628f860b4e92c3c92f1d9c782a
kuailelanhai <80752169@qq.com>
2014-07-29 10:32:46 +0800

    do the jump windows(show bid final result)

--- app/scripts/controllers/result_price.js
+++ app/scripts/controllers/result_price.js
@@ -3,25 +3,25 @@
  */
 'use strict';
 angular.module('partyBidApp')
-    .controller('Result_priceCtrl', function ($scope,$location) {
+    .controller('Result_priceCtrl', function ($scope, $location) {
         $scope.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
-        $scope.return="返回";
-        $scope.go_return=function(){
+        $scope.return = "返回";
+        $scope.go_return = function () {
             $location.path('/bid_activity')
         }
-        $scope.show_message=function(){
-            var active=JSON.parse(localStorage.getItem("messages"));
-            $scope.activity_bid=localStorage.working_bid;
+        $scope.show_message = function () {
+            var active = JSON.parse(localStorage.getItem("messages"));
+            $scope.activity_bid = localStorage.working_bid;
             console.log($scope.activity_bid)
             console.log('----------')
-            for(var i in active){
-                for(var j in active[i].bid_data){
+            for (var i in active) {
+                for (var j in active[i].bid_data) {
                     console.log('----------')
-                    if(active[i].bid_data[j].bid_name==localStorage.working_bid){
+                    if (active[i].bid_data[j].bid_name == localStorage.working_bid) {
                         console.log('+++++++++++')
                         $scope.people_number1 = active[i].bid_data[j].bid_list.length;
                         $scope.list4 = active[i].bid_data[j].bid_list;
@@ -32,13 +32,32 @@ angular.module('partyBidApp')
             }
         }
         $scope.show_message()
-
-
-
-
-
-
-
+        $scope.show_bid_message = function()
+        {
+            var action = JSON.parse(localStorage.getItem(("messages")))
+            for (var i in action) {
+                for (var j in action[i].bid_data) {
+                    for (var k in action[i].bid_data[j].bid_list) {
+                        if (action[i].bid_data[j].bid_name == localStorage.working_bid) {
+                            if (action[i].bid_data[j].bid_list[0].iprice != action[i].bid_data[j].bid_list[k].iprice) {
+                                k = 0
+                                $scope.bider = action[i].bid_data[j].bid_list[0].bid
+                                $scope.telephone = action[i].bid_data[j].bid_list[0].iphone
+                                $scope.bider_price = action[i].bid_data[j].bid_list[0].iprice
+                            }
+                            else {
+                                k = k + 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        $scope.show_bid_message()
+        $('#ModalSuccess').modal("show");
+//        $timeout(function () {
+//            $('#ModalSuccess').modal('hide');
+//        }, 3000)
 
 
     });
